c97ce914be7f444cdc7524ede9cdb0b5891a3e41
Mikhail Baranov <baranov@arrival.com>
2018-04-30 08:26:32 +0300

    Extend formatting rules

--- renderer.js
+++ renderer.js
@@ -1,10 +1,22 @@
 const marked = require('marked');
 const renderer = new marked.Renderer();
 
-renderer.heading = (text) => `${text}\n\n`;
-renderer.paragraph = (text) => `${text}\n\n`;
+const noWrap = (input) => input;
+const renderAsBlock = (input) => `${input}\n\n`;
+
+renderer.heading = renderAsBlock;
+renderer.paragraph = renderAsBlock;
 renderer.list = (body) => `${body}\n`;
-renderer.listitem = (text) => `${text}\n`;
+renderer.listitem = (text) => ` — ${text}\n`;
+renderer.code = (text) => renderAsBlock;
+renderer.blockquote = (text) => renderAsBlock(`«${text}»`);
+renderer.hr = () => '----------';
+
+renderer.codespan = noWrap;
+renderer.strong = noWrap;
+renderer.em = noWrap;
+renderer.del = noWrap;
+renderer.image = (href, title, text) => title ? `${title} — ${href}` : href;
 
 renderer.link = (href, title, text) => `${text} — ${href}`;
 
